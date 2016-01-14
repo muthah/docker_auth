98fad16584b9950da6d297b2dbf3a53cb7f219d9
Erik Veld <eveld@xebia.com>
2016-01-14 14:49:13 +0100

    Added comments

--- auth_server/authn/auth0_auth.go
+++ auth_server/authn/auth0_auth.go
@@ -1,5 +1,6 @@
 package authn
 
+// Auth0AuthConfig holds the configuration for the auth0 authenticator
 type Auth0AuthConfig struct {
 	Domain       string `yaml:"domain,omitempty"`
 	Connection   string `yaml:"connection,omitempty"`
@@ -7,23 +8,28 @@ type Auth0AuthConfig struct {
 	ClientSecret string `yaml:"client_secret,omitempty"`
 }
 
+// Auth0Auth is an auth0 authenticator
 type Auth0Auth struct {
 	config *Auth0AuthConfig
 }
 
+// NewAuth0Auth creates a new authenticator
 func NewAuth0Auth(c *Auth0AuthConfig) (*Auth0Auth, error) {
 	return &Auth0Auth{
 		config: c,
 	}, nil
 }
 
+// Authenticate user with username and password
 func (aa *Auth0Auth) Authenticate(account string, password PasswordString) (bool, error) {
 	return true, nil
 }
 
+// Stop authenticating
 func (aa *Auth0Auth) Stop() {
 }
 
+// Name of authenticator
 func (aa *Auth0Auth) Name() string {
 	return "Auth0"
 }
